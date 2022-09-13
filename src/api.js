600f7f2e38072205aa1bf1bf89035fd4f3dcd006
Unnati <unnati_k_rana@yahoo.in>
2022-09-13 16:37:31 +1000

    first commit

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b5c5924b9cmshad48f5a7ff3733fp1ee7a7jsn69ded62750fe',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
